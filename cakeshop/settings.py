d237e3dff54a8a3b6f607502c18f7b173706dea5
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-11-23 17:40:05 +0300

    Correct INSTALLED_APPS and requirements.txt

--- cakeshop/settings.py
+++ cakeshop/settings.py
@@ -25,14 +25,14 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
-    'bakery',
+    'bakery.apps.BakeryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-phonenumber-field',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
