9064d9a01a3936932364be1c94cfb5fc0241a7b7
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-11-23 17:13:45 +0300

    add cakewebsite app

--- cakeshop/urls.py
+++ cakeshop/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from cakeshop import views
+from cakewebsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
